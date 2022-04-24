b4aee3b2536a9d1a86d552fe845e31da7ac0e160
mauricio-n-bustos <mauricio.n.bustos@gmail.com>
2022-04-23 23:44:48 -0300

    Actualizacion

---
+++ figuras.js
@@ -0,0 +1,18 @@
+// Cuadrado
+
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden ${ladoCuadrado}`);
+
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perimetro del cuadrado es ${perimetroCuadrado}`);
+
+const areaCuadrada = ladoCuadrado * ladoCuadrado;
+console.log(`El area cuadrada es ${areaCuadrada}`);
+
+// Triangulo
+
+const ladoTrianguloA = 6;
+const ladoTrianguloB = 6;
+const ladoTrianguloC = 4;
+console.log(`Los lados del triangulo miden ${ladoTrianguloA}cm ${ladoTrianguloB}cm ${ladoTrianguloC}cm`);
