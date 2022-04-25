23801cbace624625a831d25261392091746bef6d
mauricio-n-bustos <mauricio.n.bustos@gmail.com>
2022-04-25 02:44:42 -0300

    Actualizacion

--- porcentajes.js
+++ porcentajes.js
@@ -1,4 +1,15 @@
-function porcentaje(precio, descuento) {    
+function porcentajeDeDescuento(precio, descuento) {    
     const precioFinal = precio *(100 - descuento) / 100;
-    console.log(precioFinal);
+    return precioFinal;
 }
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice").value;
+    const inputDiscount = document.getElementById("inputDiscount").value;
+
+    const precioConDescuento = porcentajeDeDescuento(inputPrice, inputDiscount);
+
+    const resultP = document.getElementById("resultP");
+    resultP.innerText = `El precio con decuento es: $${precioConDescuento}`;
+}
+
